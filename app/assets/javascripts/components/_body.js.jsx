4c7877193abaf08998e9d2247594cd3bd2f3610a
Lukeveille <luke@lukeleveille.com>
2018-11-05 17:55:16 -0500

    able to add valid jobs with form

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -14,19 +14,24 @@ class Body extends React.Component {
       job: {
         comments: comments,
         price: price,
+        residence_id: 4
       }
     });
+    alert(body)
     fetch('http://localhost:3000/api/v1/jobs', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: body,
-    }).then((response) => {return response.json()})
+    })
+    .then((response) => {
+      return response.json()
+    })
     .then((job) => {
       this.addNewJob(job)
     });
-  }
+}
   addNewJob(job) {
     this.setState({
       jobs: this.state.jobs.concat(job)
@@ -36,13 +41,14 @@ class Body extends React.Component {
   componentDidMount(){
     fetch('/api/v1/jobs.json')
       .then((response) => {return response.json()})
-      .then((data) => {this.setState({ jobs: data }) });
+      .then((data) => {this.setState({ jobs: data })
+    });
   }
-  render(){   
+  render(){
     return (
       <div>
         <AllJobs jobs={this.state.jobs} />
-        <NewJob handleFormSubmit={this.handleFormSubmit}/>
+        <NewJob handleFormSubmit={this.handleFormSubmit} authenticity_token={this.props.authenticity_token} />
       </div>
     )
   }
