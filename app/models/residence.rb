004f734ee83b106dbf97c49ae0e9e16b00175043
StephenVarela <varelast92@gmail.com>
2018-11-05 16:49:49 -0500

    added residence save test

--- app/models/residence.rb
+++ app/models/residence.rb
@@ -3,4 +3,11 @@ class Residence < ApplicationRecord
   has_many :driveways
   has_many :jobs
   has_many :shovelers, through: :jobs
+
+  validates :street_name, presence: true
+  validates :city_name, presence: true
+  validates :postal_code, presence: true
+  validates :country, presence: true
+  validates :is_home_address, presence: true
+
 end
--- test/models/residence_test.rb
+++ test/models/residence_test.rb
@@ -7,6 +7,14 @@ class ResidenceTest < ActiveSupport::TestCase
 
   def test_residence_factory_is_valid
 
+    my_user = create(:user)
+    my_home_owner = build(:home_owner)
+    my_home_owner.user = my_user
+    my_home_owner.save
+
+    my_residence = build(:residence)
+    my_residence.home_owner = my_home_owner
+    assert(my_residence.valid?)
   end
-  
+
 end
