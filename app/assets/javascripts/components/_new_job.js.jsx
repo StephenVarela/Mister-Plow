10f456f658a2f473723ed6c686f5724b90c0e927
Daniel Ang <dan.justin.ang@gmail.com>
2018-11-05 16:32:29 -0500

    added user sign-up

--- app/assets/javascripts/components/_new_job.js.jsx
+++ app/assets/javascripts/components/_new_job.js.jsx
@@ -1,6 +1,7 @@
 const NewJob = (props) => {
+
   let formFields = {}
- 
+
   return (
     <form>
       <input ref={input => formFields.comments = input} placeholder='Enter a comment'/>
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,34 @@
 class UsersController < ApplicationController
-  def index
-  end
-
-  def show
-  end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new
+    @user.first_name = params[:user][:first_name]
+    @user.last_name = params[:user][:last_name]
+    @user.street_name = params[:user][:street_name]
+    @user.city_name = params[:user][:city_name]
+    @user.postal_code = params[:user][:postal_code]
+    @user.country = params[:user][:country]
+    @user.email = params[:user][:email]
+    @user.primary_contact_number = params[:user][:primary_contact_number]
+    @user.secondary_contact_number = params[:user][:secondary_contact_number]
+    @user.e_wallet = params[:user][:e_wallet]
+    @user.password = params[:user][:password]
+
+    if @user.save
+      puts "Hello"
+    else
+      render :new
+    end
+    
   end
 
   def edit
   end
-  
+
   def update
   end
 
