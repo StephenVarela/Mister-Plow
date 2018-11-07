44eab47548dbd573881f3919f8e7542891715e44
StephenVarela <varelast92@gmail.com>
2018-11-07 17:58:41 -0500

    modifying user model to retrieve weather events

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,8 +19,29 @@ class User < ApplicationRecord
   validates_confirmation_of :password
   validates :password, confirmation: true
 
-  def get_weather_forecast
-    return Weather.where(city_name: "#{self.city_name}")
+  def self.get_weather_forecast
+
+    return Weather.where(city_name: "Toronto")
+    # return Weather.where(city_name: "#{self.city_name}")
+  end
+
+  def self.create_forecast_array
+    forecast = get_weather_forecast
+
+    day_1 = []
+    day_2 = []
+    day_3 = []
+    day_4 = []
+    day_5 = []
+
+    puts "========================="
+
+    forecast.each.with_index do |sample, i|
+      #puts sample
+    end
+
+    puts "========================="
+
   end
 
 end
