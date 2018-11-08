89d90815ecbd53b4d05ec75d3b94d88e64e5cf2d
Lukeveille <luke@lukeleveille.com>
2018-11-08 11:56:31 -0500

    moving user form to it's own file

--- app/assets/javascripts/components/_new_user.js.jsx
+++ app/assets/javascripts/components/_new_user.js.jsx
@@ -3,11 +3,13 @@ class NewUser extends React.Component {
     super(props);
 
     this.state = {
-      homeResidence: false,
-      addressForm: false
+      homeResidence: true,
+      addressForm: true,
+      isHomeResidence: 'checked'
     }
-    
     this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    this.residenceOption = this.residenceOption.bind(this)
+    this.homeAdress = this.homeAdress.bind(this)
   }
 
   handleFormSubmit(
@@ -23,8 +25,8 @@ class NewUser extends React.Component {
     password,
     password_confirmation,
     user_type,
-    authenticity_token
-    ) {
+    authenticity_token) {
+
     let body = JSON.stringify({
       user: {
         first_name: first_name,
@@ -68,14 +70,19 @@ class NewUser extends React.Component {
         }).then((response) => {
           return response.json()
         }).then((newHomeOwner) => {
+          let residence_street = this.state.addressForm? street_name : res_street_name
+          let residence_city = this.state.addressForm? city_name : res_city_name
+          let residence_postal = this.state.addressForm? postal_code : res_postal_code
+          let residence_country = this.state.addressForm? country : res_country
+          
           let resBody = JSON.stringify({
             residence: {
               home_owner_id: newHomeOwner.id,
-              street_name: street_name,
-              city_name: city_name,
-              postal_code: postal_code,
-              country: country,
-              is_home_address: true
+              street_name: residence_street,
+              city_name: residence_city,
+              postal_code: residence_postal,
+              country: residence_country,
+              is_home_address: this.state.addressForm
             },
             authenticity_token: authenticity_token
           });
@@ -115,55 +122,22 @@ class NewUser extends React.Component {
       }
     });
   }
-
+  residenceOption(option) {
+    if (option === 'shoveler') {
+      this.setState({homeResidence: false})
+    } else if (option === 'home_owner') {
+      this.setState({homeResidence: true})
+    }
+  }
+  homeAdress() {
+    if (this.state.addressForm === false) {
+      this.setState({isHomeResidence: 'checked', addressForm: true})
+    } else {
+      this.setState({isHomeResidence: '', addressForm: false})
+    }
+  }
 
   render() {
-    let formFields = {}
-    let homeResidence = this.state.homeResidence? <p>Residence is Home Address: <input type='checkbox' ref={input => formFields.home_address = input} name='is_home-adress' value={true} /></p> : ''
-    let addressForm = ''
-
-    return (
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        this.handleFormSubmit(
-            formFields.first_name.value,
-            formFields.last_name.value,
-            formFields.street_name.value,
-            formFields.city_name.value,
-            formFields.postal_code.value,
-            formFields.country.value,
-            formFields.email.value,
-            formFields.primary_contact_number.value,
-            formFields.secondary_contact_number.value,
-            formFields.password.value,
-            formFields.password_confirmation.value,
-            formFields.user_type.value,
-            formFields.authenticity_token.value,
-          );
-          e.target.reset();        
-        }}>
-        <p><input ref={input => formFields.first_name = input} placeholder='First Name'/></p>
-        <p><input ref={input => formFields.last_name = input} placeholder='Last Name' /></p>
-        <p><input ref={input => formFields.street_name = input} placeholder='Street Name' /></p>
-        <p><input ref={input => formFields.city_name = input} placeholder='City Name' /></p>
-        <p><input ref={input => formFields.postal_code = input} placeholder='Postal Code' /></p>
-        <p><input ref={input => formFields.country = input} placeholder='Country' /></p>
-        <p><input ref={input => formFields.email = input} placeholder='E-mail' /></p>
-        <p><input ref={input => formFields.primary_contact_number = input} placeholder='Primary Contact Number' /></p>
-        <p><input ref={input => formFields.secondary_contact_number = input} placeholder='Secondary Contact Number' /></p>
-        <p><input type='password' ref={input => formFields.password = input} placeholder='Password' /></p>
-        <p><input type='password' ref={input => formFields.password_confirmation = input} placeholder='Password Confirmation' /></p>
-        <p>I am a: <select ref={input => formFields.user_type = input}>
-          <option value="home_owner">Homeowner</option>  
-          <option value="shoveler">Shoveler</option>  
-        </select></p>
-        
-        {homeResidence}
-        {addressForm}
-
-        <input ref={input => formFields.authenticity_token = input} type='hidden' name='authenticity_token' value={this.props.authenticity_token} />
-        <p><button type='submit'>Sign Up</button></p>
-      </form>
-    )
+    <NewUserForm homeResidence={this.state.homeResidence}/>
   }
 }
