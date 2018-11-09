f4fcba1920658059767fc5fae1df30678a9f2273
Lukeveille <luke@lukeleveille.com>
2018-11-09 17:27:19 -0500

    set up booking display

--- app/assets/javascripts/components/_app.js.jsx
+++ app/assets/javascripts/components/_app.js.jsx
@@ -4,18 +4,20 @@ class App extends React.Component{
     super(props);
     this.state = {
       jobs: [],
-      residence: 0
+      residence: 0,
+      bookingReady: false,
     };
 
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleJobCreate = this.handleJobCreate.bind(this);
     this.addNewJob = this.addNewJob.bind(this)
+    this.bookingSwitch = this.bookingSwitch.bind(this)
   }
 
-  handleFormSubmit(comments, price, day, time, authenticity_token) {
+  handleJobCreate(comments, day, time, authenticity_token) {
     let body = JSON.stringify({
       job: {
         comments: comments,
-        price: price,
+        price: 15,
         scheduled_time: new Date(day + ' ' + time).getTime(),
         residence_id: this.props.user.residences[this.state.residence].id
       },
@@ -36,10 +38,27 @@ class App extends React.Component{
       this.addNewJob(job)
     });
   }
+
+
+
+
+
+
+
+
+
+
   addNewJob(job) {
     this.setState({
-      jobs: this.state.jobs.concat(job)
+      jobs: this.state.jobs.concat(job),
     });
+  }
+
+  bookingSwitch() {
+    this.setState({
+      bookingReady: true,
+    });
+    console.log("I'm here");
   }
   componentDidMount(){
     fetch('/api/v1/jobs.json')
@@ -47,9 +66,9 @@ class App extends React.Component{
       .then((data) => {this.setState({ jobs: data })
     });
   }
+
   render(){
-    console.log(this.props.user.current_user)
-    let userWidget = this.props.user.current_user.is_shoveler? <MapView /> : <NewJob handleFormSubmit={this.handleFormSubmit} authenticity_token={this.props.authenticity_token} />;
+    let userWidget = this.props.user.current_user.is_shoveler? <MapView /> : <NewJob bookingSwitch={this.bookingSwitch} bookingReady={this.state.bookingReady} handleJobCreate={this.handleJobCreate} residence={this.props.user.residences[this.state.residence]} authenticity_token={this.props.authenticity_token} />;
 
     return (
       <div>
--- app/assets/javascripts/components/_new_job.js.jsx
+++ app/assets/javascripts/components/_new_job.js.jsx
@@ -1,16 +1,38 @@
 const NewJob = (props) => {
 
   let formFields = {}
+  if (props.bookingReady) {
+    var bookingDisplay = <form className="job-form" onSubmit={() => {props.handleJobCreate(formFields.comments.value, formFields.price.value, formFields.day.value, formFields.time.value, formFields.authenticity_token.value)}}>
+    <p>Date: <input ref={input => formFields.day = input} type='date' placeholder='Scheduled day' /></p>
+    <p>Finished by: <input ref={input => formFields.time = input} type='time' placeholder='Scheduled time' /></p>
+    <p>Notes: <textarea ref={input => formFields.comments = input} placeholder='Enter any notes or special instructions'/></p>
+    <input ref={input => formFields.authenticity_token = input} type='hidden' name='authenticity_token' value={props.authenticity_token} />
+    <button>Submit</button>
+    </form>
+  } else {
+    var bookingDisplay = <button className="booking-button" onClick={() => {props.bookingSwitch()}}>Book Now!</button>
+  }
 
   return (
-    <form className="job-form" onSubmit={() => {props.handleFormSubmit(formFields.comments.value, formFields.price.value, formFields.day.value, formFields.time.value, formFields.authenticity_token.value)}}>
+    <div className="job-booking" >
       <h2>Book a New Job</h2>
-      <p><input ref={input => formFields.comments = input} placeholder='Enter a comment'/></p>
-      <p><input ref={input => formFields.price = input} placeholder='Enter a price' /></p>
-      <p><input ref={input => formFields.day = input} type='date' placeholder='Scheduled day' /></p>
-      <p><input ref={input => formFields.time = input} type='time' placeholder='Scheduled time' /></p>
-      <input ref={input => formFields.authenticity_token = input} type='hidden' name='authenticity_token' value={props.authenticity_token} />
-      <button>Submit</button>
-    </form>
+      <div className="job-booking-display">
+
+        <div className="residence-details">
+          <div className="residence-pic-border">
+            <img className="residence-pic"/>
+          </div>
+
+          <ul>
+            <li><h2>{props.residence.street_name}</h2></li>
+            <li><h2>{props.residence.city_name}</h2></li>
+            <li><h2>{props.residence.postal_code}</h2></li>
+          </ul>
+        </div>
+
+        {bookingDisplay}
+
+      </div>
+    </div>
   )
 }
