3aef5742a5ea97df9c76001ae05f9cc044cd99cf
StephenVarela <varelast92@gmail.com>
2018-11-10 11:38:57 -0500

    added environment variables and configuration for s3 bucket

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,7 +2,13 @@ class Job < ApplicationRecord
   belongs_to :shoveler, optional: true
   belongs_to :residence
 
+  has_one_attached :confirmation_picture
+
   validates :residence_id, presence: true
   validates :price, presence: true
   validates :scheduled_time, presence: true
+end
+
+def attach_confirmation(picture)
+  confirmation_picture.attach(picture)
 end
