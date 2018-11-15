d82784f97d7f6b2d18f116a3880ee1e9a950bf4f
StephenVarela <varelast92@gmail.com>
2018-11-15 17:03:00 -0500

    added map logic to plot accepted jobs in green and available in red

--- app/assets/javascripts/components/App.js.jsx
+++ app/assets/javascripts/components/App.js.jsx
@@ -254,7 +254,7 @@ class App extends React.Component{
     });
   }
   render(){
-    let userWidget = this.props.user.current_user.is_shoveler? <MapView residences={this.props.user.job_residences} /> : <NewJob showBookingForm={this.showBookingForm} bookingForm={this.state.bookingForm} handleJobCreate={this.handleJobCreate} residence={this.props.user.residences[this.state.residence]} authenticity_token={this.props.authenticity_token} />;
+    let userWidget = this.props.user.current_user.is_shoveler? <MapView residences={this.props.user.job_residences} state={this.state} /> : <NewJob showBookingForm={this.showBookingForm} bookingForm={this.state.bookingForm} handleJobCreate={this.handleJobCreate} residence={this.props.user.residences[this.state.residence]} authenticity_token={this.props.authenticity_token} />;
 
     let dashboard = [<AllJobs showCompletedJobs={this.showCompletedJobs} residenceIndex={this.residenceIndex} timeString={this.timeString} dateString={this.dateString} completedJobsModal={this.state.completedJobsModal} jobConfirmation={this.jobConfirmation} jobComplete={this.jobComplete}checkIn={this.checkIn} jobModalSwitchOff={this.jobModalSwitchOff} jobModalSwitchOn={this.jobModalSwitchOn} jobModal={this.state.jobModal} acceptJob={this.acceptJob} jobDetails={this.jobDetails} residences={this.props.user.job_residences} availableJobs={this.state.availableJobs} bookedJobs={this.state.bookedJobs} completedJobs={this.state.completedJobs} jobs={this.state.jobs} user={this.props.user} key="all-jobs" />, <WeatherApp key="weather-app" weekdayString={this.weekdayString} dateString={this.dateString} timeString={this.timeString} />];
     let dashboardArrangment = this.props.user.current_user.is_shoveler? dashboard : dashboard.reverse();
--- app/assets/javascripts/components/MapView.js.jsx
+++ app/assets/javascripts/components/MapView.js.jsx
@@ -4,26 +4,17 @@ class MapView extends React.Component {
     super(props)
 
   }
-
   componentDidMount(e){
-
-
-    var mymap = L.map('mapid').setView([43.6532, -79.3832], 15);
+    mymap = L.map('mapid').setView([43.6532, -79.3832], 15);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       id: 'mapbox.streets',
       accessToken: 'pk.eyJ1Ijoic3RlcGhlbnZhcmVsYSIsImEiOiJjam85a2VkaHgwMXRjM2xvMzN2bXprMDI4In0._pTIvoLYIVcIFPRQIQjKHA'
     }).addTo(mymap);
-
-    var findButton = document.querySelector('#GeoFind')
-
-    findButton.addEventListener('click', () => {this.clickMapButton(this.props.residences, mymap)})
-
-
   }
 
-  clickMapButton(e, mymap){
+  clickMapButton(state, residences, mymap){
     var output = document.getElementById("out");
     if (!navigator.geolocation){
       output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
@@ -33,19 +24,43 @@ class MapView extends React.Component {
       var latitude  = position.coords.latitude;
       var longitude = position.coords.longitude;
 
-      output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+      output.innerHTML = '';
       var marker = L.marker([latitude, longitude]).addTo(mymap);
       marker.bindPopup("<b>You Are Here</b>").openPopup();
 
-      e.forEach(function(location){
-        var circle = L.circle([location.lat, location.lon], {
-          color: 'red',
-          fillColor: '#f03',
-          fillOpacity: 0.5,
-          radius: 20
-        }).addTo(mymap);
-        circle.bindPopup("<img src=https://image.flaticon.com/icons/svg/86/86364.svg>")
-      })
+      var residence_ids = []
+      state.bookedJobs.forEach(function(job){
+        if(job.shoveler_id){
+          residence_ids.push(job.residence_id)
+        }
+      });
+
+      residences.forEach(function(residence){
+        if(residence_ids.includes(residence.id)){
+          console.log("GOOD")
+            var accepted = L.circle([residence.lat, residence.lon], {
+              color: 'green',
+              fillColor: 'green',
+              fillOpacity: 0.5,
+              radius: 20
+            }).addTo(mymap);
+          accepted.bindPopup("<img src=https://image.flaticon.com/icons/svg/86/86364.svg>")
+        }else{
+          console.log("BAD")
+          var available = L.circle([residence.lat, residence.lon], {
+            color: 'red',
+            fillColor: 'red',
+            fillOpacity: 0.5,
+            radius: 20
+          }).addTo(mymap);
+        }
+        });
+
+
+      // state.bookedJobs.forEach(function(job){
+      //
+      //   console.log(job)
+
 
     }
     function error() {
@@ -60,7 +75,7 @@ class MapView extends React.Component {
       <div className="map-view">
         <h1>Your Map</h1>
         <div id="mapid"></div>
-        <p><button id="GeoFind">Show Locations</button></p>
+        <p><button id="GeoFind" onClick={() => {this.clickMapButton(this.props.state, this.props.residences, mymap)}}>Show Locations</button></p>
         <div id="out"></div>
       </div>
     )
